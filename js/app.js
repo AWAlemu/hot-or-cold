0f644f52f453c008a81ca19196b4a30c233eb4bd
Alex Weldetsadik <alexweldetsadik@gmail.com>
2016-07-10 15:56:10 -0400

    Interactive version

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 
 $(document).ready(function(){
 	
+	//Start a new game on page load
+	number = newGame();
+	counter = 0;
+
+	//Start a new game when start page selected
+	$('.new').click(function() {
+		number = newGame();
+	});
+
+	//User guess event handler 
+	$('#guessButton').click(function() {
+		var input = validateInput();
+		$('#userGuess').val('');
+		if (input) {
+			$('#guessList').append('<li>' + input + '</li>');
+			feedback(input, number);
+		}
+	});
+ 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +31,52 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	
+
 });
 
 
+function generateNumber() {
+	var num = Math.floor((Math.random() * 100) + 1);
+	return num;
+}
+
+function validateInput() {
+	var input = $('input[name=userGuess').val();
+	if (isNaN(input) || input < 1 || input > 100) {
+		alert('Please Enter a Number Between 1 and 100');
+	}
+	else {
+		return input;
+	}
+}
+function newGame() {
+	reset();
+	return generateNumber();
+}
+function feedback(guess, number) {
+	var nmbr = number;
+	if (guess <= nmbr - 50 || guess >= nmbr + 50) {
+		$('h2#feedback').html('Ice Cold');
+	}
+	else if (guess <= nmbr - 30 || guess >= nmbr + 30) {
+		$('h2#feedback').html('Cold');
+	}
+	else if (guess <= nmbr - 20 || guess >= nmbr + 20) {
+		$('h2#feedback').html('Warm');
+	}	
+	else if (guess <= nmbr - 10 || guess >= nmbr + 10) {
+		$('h2#feedback').html('Hot');
+	}
+	else if (guess > nmbr - 10 || guess < nmbr + 10) {
+		$('h2#feedback').html('Very Hot');
+	}
+	counter++;
+	$('span#count').html(counter);
+}
+function reset() {
+	$('h2#feedback').html('Make your Guess!');
+	counter = 0;
+	$('span#count').html(counter);
+	$('#guessList').empty();
+}
